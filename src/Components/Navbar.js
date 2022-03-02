7dc1a098abe635fffbd2858f6ad5390dc5d09238
danishm4557 <danishm4557@gmail.com>
2022-03-02 15:28:02 -0600

    added navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
-export default class Navbar extends Component {
+export default class NavBar extends Component {
   constructor(props) {
     super(props);
   }
@@ -8,8 +10,45 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <h1 className="text-3xl font-bold">Navbar</h1>
-        <p>Navbar</p>
+        <Navbar className="nav-bar" bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Trek</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Trending</Nav.Link>
+                <NavDropdown title="😎" id="basic-nav-dropdown">
+                  <NavDropdown.Item
+                    className="NavDropdownItem"
+                    href="#action/3.1"
+                  >
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="NavDropdownItem"
+                    href="#action/3.2"
+                  >
+                    My Calendar 📅
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="NavDropdownItem"
+                    href="#action/3.3"
+                  >
+                    My Posts 🌄
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    className="NavDropdownItem"
+                    href="#action/3.4"
+                  >
+                    Sign Out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </>
     );
   }
