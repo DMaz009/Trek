f5fad6a9da6f0639b62a23dc25ade1141fe9fcc9
danishm4557 <danishm4557@gmail.com>
2022-03-02 13:21:53 -0600

    still building the structure

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Navbar</h1>
+      </>
+    );
+  }
+}
