1923d73db73a9047c7f25fa42bcfd2492dafdc03
David Mazza <DavidMazza@Davids-MBP.attlocal.net>
2022-03-02 15:06:02 -0600

    base form for Searchbox

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -8,7 +8,21 @@ export default class SearchBox extends Component {
   render() {
     return (
       <>
-        <h1>SearchBox</h1>
+        <br/>
+        <h1>Plan A Trip</h1>
+        <form onSubmit="submitFunction" className="search-box">
+          <label className="destination-label">Destination</label>
+          <input className="search-input" name="citySearch" type="text" onChange="cityChange" />
+          <label className="state-date-label">Start Date </label>
+          <input className="date-input" name="startDate" type="date" onChange="startDateChange" />
+          <label className="end-date-label">End Date </label>
+          <input className="date-input" name="endDate" type="date" onChange="endDateChange" />
+
+          <br/>
+          <input className="submit-input" name="search-button" type="submit" value='Search Button'/>
+
+
+        </form>
       </>
     );
   }
